c34b8c803af7330b1f26125ef035137f0b56609f
Alex <git@alexnewark.co.uk>
2018-07-30 14:24:02 +0100

    Updated

--- viewport-units.js
+++ viewport-units.js
@@ -1,15 +1,13 @@
-'use strict';
+import {Dimensions} from 'react-native'
 
-var React = require('react-native')
-  , Dimensions = React.Dimensions || require('Dimensions')
-  , {width, height} = Dimensions.get('window');
+var {width, height} = Dimensions.get('window');
 
 var units = {
-  vw: width/100
-, vh: height/100
-};
+  vw: width/100,
+  vh: height/100
+}
 
-units.vmin = Math.min(units.vw, units.vh);
-units.vmax = Math.max(units.vw, units.vh);
+units.vmin = Math.min(units.vw, units.vh)
+units.vmax = Math.max(units.vw, units.vh)
 
-module.exports = units;
+export default units;
